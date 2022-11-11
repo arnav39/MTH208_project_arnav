8368c5a7d47721a17758f3ed10e8cd010fd45d58
Chhavi21 <110392860+Chhavi21@users.noreply.github.com>
2022-11-11 10:21:20 +0530

    Update Host cities for Summer and Winter Olympic Games.R

--- group-project-group-22/DataExtraction/Host cities for Summer and Winter Olympic Games.R
+++ group-project-group-22/DataExtraction/Host cities for Summer and Winter Olympic Games.R
@@ -1,6 +1,10 @@
 library(rvest)
-html <- read_html("https://en.wikipedia.org/wiki/List_of_Olympic_Games_host_cities")
-tab <- html_table(html)
+
+tab <- read_html("https://en.wikipedia.org/wiki/List_of_Olympic_Games_host_cities") %>% 
+  html_table()
 
 tab1 <- tab[[2]]
+tab1 <- tab1[ , 3:4]
+
 tab2 <- tab[[3]]
+tab2 <- tab2[ , 3:6]
